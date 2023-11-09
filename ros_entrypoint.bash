822cf2634d32a82eda0f57463a294dda3bf98993
Celeste Pilgrim <joeuser846@gmail.com>
2023-11-09 02:14:41 -0500

    bash echo cleanup

--- ros_entrypoint.bash
+++ ros_entrypoint.bash
@@ -3,10 +3,10 @@
 source devel/setup.bash
 if [ $ROS_HOSTNAME == "rvrbot" ]
 then
-	echo ">>>>> RUNNING AS RVRBOT"
+	echo ">>>>> RUNNING AS RVRBOT <<<<<"
     roslaunch sphero_rvr_bringup sphero_rvr_merged_bringup.launch
 else
-	echo ">>>>> RUNNING AS DEVHOST"
+	echo ">>>>> RUNNING AS DEVHOST <<<<<"
     roslaunch sphero_rvr_navigation sphero_rvr_navigation.launch
 fi
 
