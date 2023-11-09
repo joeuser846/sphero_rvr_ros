e3c3b94d4c4bb83184bbe0fbb6fa07a6f20723cf
Celeste Pilgrim <joeuser846@gmail.com>
2023-11-09 02:01:34 -0500

    Automate roslaunch depending on ROS_HOSTNAME

--- ros_entrypoint.bash
+++ ros_entrypoint.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 source devel/setup.bash
+if [ $ROS_HOSTNAME == "rvrbot" ]
+then
+	echo ">>>>> RUNNING AS RVRBOT"
+    roslaunch sphero_rvr_bringup sphero_rvr_merged_bringup.launch
+else
+	echo ">>>>> RUNNING AS DEVHOST"
+    roslaunch sphero_rvr_navigation sphero_rvr_navigation.launch
+fi
 
 # Execute CMD parameter if we get here
 exec "$@"
