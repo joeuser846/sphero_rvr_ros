9e957e3285edd746997adedd378ad1c0a9ca0428
Celeste Pilgrim <joeuser846@gmail.com>
2023-12-03 18:33:12 -0500

    Fix navigation launch in entrypoint

--- ros_entrypoint.bash
+++ ros_entrypoint.bash
@@ -7,7 +7,7 @@ then
     roslaunch sphero_rvr_bringup sphero_rvr_bringup.launch
 else
 	echo ">>>>> RUNNING AS DEVHOST <<<<<"
-    roslaunch sphero_rvr_navigation navigation.launch
+    roslaunch sphero_rvr_navigation sphero_rvr_navigation.launch
 fi
 
 # Execute CMD parameter if we get here
